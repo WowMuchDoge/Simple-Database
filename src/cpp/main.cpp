5d9e65d2d350425a4fa45f73518ae67d51986084
WowMuchDoge <nolanthw8s@gmail.com>
2024-01-14 10:55:28 -0600

    Fix scanner class

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "../include/ColumnHead.h"
 #include "../include/TableHead.h"
@@ -7,16 +8,5 @@
 #include "../include/Token.h"
 
 int main() {
-    Scanner scanner("ADD_COLUMN(DOUBLE, 'xjfjkgf')  ADD_COLUMN(DOUBLE, 'asdf')  ADD_ROW(69.420, 2345.2)  ADD_ROW(692.410, 23439.1)  GET_ELEMENT('asdf', 1)");
-
-    TableHead head;
-
-    std::vector<Token> tokens = scanner.scanTokens();
-
-    for (Token token : tokens) {
-        std::cout << token.type << ", " << token.value << '\n';
-    }
-
-    Parser parser(tokens, &head);
-    parser.parse();
+    
 }
