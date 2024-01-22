eca5529eb5cb390af337f37dee37d26ac2a61172
Nolan T <97766806+WowMuchDoge@users.noreply.github.com>
2024-01-22 07:52:55 -0600

    Fix TableHead saving (#27)
    * Add windows colors for lexical errors
    
    * Fix lexical error colors for windows
    
    * Remove colored errors for windows
    
    * Fix Makefile
    
    * Add macro to bold if not on linux
    
    * Fix tablhead saving

--- src/cpp/TableHead.cpp
+++ src/cpp/TableHead.cpp
@@ -68,25 +68,23 @@ void TableHead::writeToFile(std::string fileName) {
         int j = 0;
         for (int i = 0; i < columns[0]->rowLen(); i++) {
             file << "ADD_ROW(";
-            j = 0;
-            for (int i = 0; i < columns.size(); i++) {
-                if (columns[i]->getTypeName() == "i") {
-                    file << ((ColumnHead<int>*)columns[i].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
-                } else if (columns[i]->getTypeName() == "NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE") {
-                    file << ((ColumnHead<std::string>*)columns[i].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
-                } else if (columns[i]->getTypeName() == "d") {
-                    file << ((ColumnHead<double>*)columns[i].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
-                } else if (columns[i]->getTypeName() == "b") {
-                    if (((ColumnHead<bool>*)columns[i].get())->getElement(i) == true) {
+            for (int j = 0; j < columns.size(); j++) {
+                if (columns[j]->getTypeName() == "i") {
+                    file << ((ColumnHead<int>*)columns[j].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
+                } else if (columns[j]->getTypeName() == "NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE") {
+                    file << ((ColumnHead<std::string>*)columns[j].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
+                } else if (columns[j]->getTypeName() == "d") {
+                    file << ((ColumnHead<double>*)columns[j].get())->getElement(i) << (j == columns.size() - 1 ? "" : " ");
+                } else if (columns[j]->getTypeName() == "b") {
+                    if (((ColumnHead<bool>*)columns[j].get())->getElement(i) == true) {
                         file << "TRUE" << (j == columns.size() - 1 ? "" : " ");
-                    } else if (((ColumnHead<bool>*)columns[i].get())->getElement(i) == false) {
+                    } else if (((ColumnHead<bool>*)columns[j].get())->getElement(i) == false) {
                         file << "FALSE" << (j == columns.size() - 1 ? "" : " ");
                     }
                 } else {
                     file.close();
                     exit(UNKOWN_TYPE);
                 }
-                j++;
             }
             file << ")\n";
         }
