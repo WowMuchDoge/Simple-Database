e350b1c370183c474058a85a04c57330e6a9d242
WowMuchDoge <nolanthw8s@gmail.com>
2024-01-22 12:05:47 -0600

    Update help text

--- src/include/Constants.h
+++ src/include/Constants.h
@@ -12,6 +12,8 @@
 
 #define FILE_NOT_OPEN 40
 
+#define NAME_QUOTE std::string(1, '"') + "<name>" + std::string(1, '"')
+
 #define WHITE std::string("\e[0;37m")
 #define RED std::string("\e[0;31m")
 #define BOLD std::string("\e[1;37m")
@@ -21,14 +23,18 @@ namespace StringConstants {
     const std::string helpText = "\n                == COMMANDS ==\n\n"
     "save <filename>: Save current database state to a text file\n"
     "load <filename>: Load another database state\n"
+    "clear row: Clears all of the rows\n"
+    "clear columns: Clears entire state\n"
     "exit: Exits out of the program\n\n"
     "            == DATABASE METHODS ==\n\n"
-    "ADD_COLUMN(<type> " + std::string(1, '"') + "<name>" + std::string(1, '"') + "): Adds a column with the specified name\n\n"
+    "ADD_COLUMN(<type> " + NAME_QUOTE + "): Adds a column with the specified name\n\n"
     "ADD_ROW(<element>...): Add a row with n elements (amount and type of elements depend on the corresponding column)\n\n"
-    "GET_ELEMENT(" + std::string(1, '"') + "<name>" + std::string(1, '"') + " <index>): Prints the element at column <name> and row <index>\n\n"
+    "GET_ELEMENT(" + NAME_QUOTE + " <index>): Prints the element at column <name> and row <index>\n\n"
     "EDIT_ROW(<index> <element>...): Replace a row at row <index>\n\n"
-    "REMOVE_COLUMN(<index>): Removes a column at index <index>\n\n"
-    "REMOVE_ROW(<index>): Removes a column at index <index>\n\n\n"
+    "REMOVE_COLUMN(" + NAME_QUOTE + "): Removes a column at index <index>\n\n"
+    "REMOVE_ROW(<index>): Removes a column at index <index>\n\n"
+    "EDIT_ELEMENT(" + NAME_QUOTE + " <index>): Edits the element at column <name> and row <index>\n\n"
+    "EDIT_COLNAME(" + std::string(1, '"') + "<name>" + std::string(1, '"') + " " + std::string(1, '"') + "<changed name>" + std::string(1, '"') + "): Edits the column name of column <name> and changes the name to <changed name>\n\n\n"
     "                == TYPES ==\n\n\n"
     "INT: A 32 bit signed integer\n\n"
     "BOOL: A boolean value of TRUE or FALSE\n\n"
